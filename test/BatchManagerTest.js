5a3e3faed5fd853068b7584a74c6ba7cd70b983c
jeanire <mbezele@ad.guichetunique.cm>
2022-05-24 19:32:05 +0100

    add batchTest

--- test/BatchManagerTest.js
+++ test/BatchManagerTest.js
@@ -9,20 +9,23 @@ contract("BatchManager", async accounts => {
     // console.log(await instance.getBatch(0));
     // console.log(await instance.getBatch(1));
     // console.log(await instance.getBatch(2));
-  // const result = await instance.createAppendBatch(" Ne Batch 1",false, {from : accounts[0]});
-  // assert.ok(result.receipt.status,'result ok');
     
-    assert.equal(web3.utils.fromWei(await instance.getTotalDepositedInAllBatch( {from : accounts[0]})), '50000','Montant inférieur');
+  console.log(web3.utils.fromWei(await instance.getBatchListSize()));
+  const result1 = await instance.createAppendBatch("BATCH-TEST 4 ",false, {from : accounts[0]});
+  console.log(web3.utils.fromWei(await instance.getBatchListSize()));
+  assert.ok(result1.receipt.status,'result ok');
+    
+    // assert.equal(web3.utils.fromWei(await instance.getTotalDepositedInAllBatch( {from : accounts[0]})), '50000','Montant inférieur');
     // assert.equal(web3.utils.fromWei(await instance.getTotalInLockedBatch( accounts[0])), 0,'Montant inférieur');
     // assert.equal(web3.utils.fromWei(await instance.getUserWeight(  accounts[0])), '100000','Montant inférieur');
     //assert.equal(web3.utils.fromWei(await instance.getUserWeight(accounts[0])), '50000','Montant inférieur');
     //assert.equal(await instance.recoverLostWallet(accounts[0],accounts[1],{from : accounts[0]}), '50000','Montant inférieur');
     
      //assert.equal(await instance.getBatchListSize(), '2','nbre de bactch non trouvé');
-     assert.equal(await instance.getBatchListSize(), 2);
-     const result = await instance.createAppendBatch("Batch 3 |Batch test", false, {from: accounts[0]});
-     assert.ok(result.receipt.status)
-     assert.equal(await instance.getBatchListSize(), 3);
+      // assert.equal(await instance.getBatchListSize(), 2);
+      //const result = await instance.createAppendBatch("Batch 3 |Batch test", false, {from: accounts[0]});
+      //assert.ok(result.receipt.status)
+      //assert.equal(await instance.getBatchListSize(), 3);
     
   });
   
